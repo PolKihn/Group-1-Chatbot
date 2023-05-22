0aac24078940f159ed2e4175cb1c37828116a3ca
Pol Kihn <polkihn@gmail.com>
2023-05-22 22:28:09 +0200

    Added wrapping function and comments
    Improve readability of the code and the output.

--- Code/client.py
+++ Code/client.py
@@ -2,19 +2,59 @@ import socket
 import colorama
 from colorama import Fore, Style
 
+def wrap(str, width):
+    paragraphs = str.split("\n\n")
+    lines = []
+    words = []
+    text = ""
+    temp = ""
+    terminalWidth = width
+    for paragraph in paragraphs:
+        lines = paragraph.split("\n")
+        for line in lines:
+            words = line.split()
+            for word in words:
+                if len(temp) + len(word) < terminalWidth:
+                    if temp == "":
+                        temp = word
+                    else:
+                        temp = temp + " " + word
+                elif len(word) > terminalWidth:
+                    text = text + temp + "\n" + word + "\n"
+                    temp = ""
+                else:
+                    text = text + temp + "\n"
+                    temp = word
+            if temp != "":
+                text = text + temp + "\n"
+            temp = ""
+        text = text + "\n\n"
+    return text
+
+
+# Set up connection
 SERVER = "127.0.0.1"
 PORT = 10815 
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((SERVER, PORT))
+
+# Chat window
 print(Fore.RED+Style.BRIGHT+"Is there anything you would like to know about BINFO? (Type EXIT to close)")
 while True:
+    
+    # Get input
     userInput = input(Fore.BLUE+"Question: ")
 
-    if userInput == "EXIT":
+    if userInput.lower() == "exit":
         s.close()
         break
 
+    # Send and receive message
     s.sendall(userInput.encode())
     response = s.recv(1024).decode()
-    print(Fore.GREEN+f"BINFO Info Bot: {response}")
+    response =f"BINFO Info Bot: \n{response}"
+    
+    response = wrap(response, 80)
+
+    # output response
+    print(Fore.GREEN+response)
